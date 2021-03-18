206d9ad7d5ba40f6e5aa805e9da78efbd31fe483
Shiva Omprakash <55973129+sivashanmugamo@users.noreply.github.com>
2021-03-18 13:28:52 -0400

    Added method experimenting py files

---
+++ dense_test.py
@@ -0,0 +1,31 @@
+'''
+Author: @sivashanmugamo
+'''
+
+import cv2 as cv
+import numpy as np
+
+# cap = cv.VideoCapture("OF/data/test.mp4")
+cap= cv.VideoCapture('OF/data/japan_crossing.mp4')
+ret, first_frame = cap.read()
+prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
+mask = np.zeros_like(first_frame)
+mask[..., 1] = 255
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    cv.imshow('Input Video Feed', frame)
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    flow_vector = cv.calcOpticalFlowFarneback(prev_gray, gray_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    magnitude, angle = cv.cartToPolar(flow_vector[..., 0], flow_vector[..., 1])
+    mask[..., 0] = angle * 180 / np.pi / 2
+    mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+    rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR)
+    cv.imshow('Dense Optical Flow', rgb)
+    prev_gray = gray_frame
+    
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
---
+++ sparse_test.py
@@ -0,0 +1,40 @@
+'''
+Author: @sivashanmugamo
+'''
+
+import cv2 as cv
+import numpy as np
+
+feature_parameters = dict(maxCorners = 300, qualityLevel = 0.2, minDistance = 2, blockSize = 7)
+filter_parameters = dict(winSize = (15,15), maxLevel = 2, criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+
+# cap = cv.VideoCapture("OF/data/test.mp4")
+cap= cv.VideoCapture('OF/data/japan_crossing.mp4')
+color= (0, 255, 0)
+ret, first_frame= cap.read()
+prev_gray_frame= cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
+prev_frame= cv.goodFeaturesToTrack(prev_gray_frame, mask = None, **feature_parameters)
+mask= np.zeros_like(first_frame)
+
+while(cap.isOpened()):
+    ret, frame= cap.read()
+    cv.imshow('Input Video Feed', frame)
+    gray_frame= cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    next, status, error = cv.calcOpticalFlowPyrLK(prev_gray_frame, gray_frame, prev_frame, None, **filter_parameters)
+    good_old= prev_frame[status == 1]
+    good_new= next[status == 1]
+    for i, (new, old) in enumerate(zip(good_new, good_old)):
+        a, b= new.ravel()
+        c, d= old.ravel()
+        mask= cv.line(mask, (a, b), (c, d), color, 2)
+        frame= cv.circle(frame, (a, b), 3, color, -1)
+    output= cv.add(frame, mask)
+    prev_gray_frame= gray_frame.copy()
+    prev_frame= good_new.reshape(-1, 1, 2)
+    cv.imshow("Sparse OF", output)
+    
+    if cv.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
